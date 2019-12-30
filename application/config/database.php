57b2d039794203a434c8fd5ca87d6469bfb17941
alfikridotname <alfikri.name@gmail.com>
2019-12-30 10:40:14 +0700

    Update Data Siswa

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,31 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'root',
 	'password' => '',
-	'database' => '',
+	'database' => 'dbrumahku',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['tes'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'tes',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,101 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-	<style type="text/css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+    	<br>
+    	<div class="row">
+    		<div class="col-md-8">
+		    	<div class="card">
+				  <div class="card-header">
+				    Data Siswa
+				  </div>
+				  <div class="card-body">
+				    <form action="">
+				    	<div class="form-group">
+				    		<label for="nis">NIS</label>
+				    		<input type="text" id="nis" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="nama">NAMA</label>
+				    		<input type="text" id="nama" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="jenkel">Jenis Kelamin</label>
+				    		<select name="jenkel" id="jenkel" class="form-control">
+				    			<option value="L">Laki-Laki</option>
+				    			<option value="P">Perempuan</option>
+				    		</select>
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="tempat_lahir">Tempat Lahir</label>
+				    		<input type="text" id="tempat_lahir" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="tanggal_lahir">Tanggal Lahir</label>
+				    		<input type="date" id="tanggal_lahir" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="alamat">Alamat</label>
+				    		<input type="text" id="alamat" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="nama_ayah">Nama Ayah</label>
+				    		<input type="text" id="nama_ayah" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="tempat_lahir_ayah">Tempat Lahir</label>
+				    		<input type="text" id="tempat_lahir_ayah" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="tanggal_lahir_ayah">Tanggal Lahir</label>
+				    		<input type="date" id="tanggal_lahir_ayah" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="pekerjaan_ayah">Pekerjaan</label>
+				    		<input type="text" id="pekerjaan_ayah" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="nama_ibu">Nama Ibu</label>
+				    		<input type="text" id="nama_ibu" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="tempat_lahir_ibu">Tempat Lahir</label>
+				    		<input type="text" id="tempat_lahir_ibu" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="tanggal_lahir_ibu">Tanggal Lahir</label>
+				    		<input type="date" id="tanggal_lahir_ibu" class="form-control">
+				    	</div>
+				    	<div class="form-group">
+				    		<label for="pekerjaan_ibu">Pekerjaan</label>
+				    		<input type="text" id="pekerjaan_ibu" class="form-control">
+				    	</div>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+				    	<button type="submit" id="btnSimpan" class="btn btn-primary">Simpan</button>
+				    </form>
+				  </div>
+				</div>
+			</div>
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
+			<div class="col-md-4">
+			</div>
+		</div>
+    </div>
 
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
-</head>
-<body>
-
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
-
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
-
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
-
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
 </html>
